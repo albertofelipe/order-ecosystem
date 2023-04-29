92a205e3c58ff531d7850dfabd58a5483a639adb
Alberto Felipe <felipe0032sena@gmail.com>
2023-04-29 11:00:47 -0300

    Project created

---
+++ src/main/java/com/projectalberto/projectapispring/ProjectApiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.projectalberto.projectapispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApiSpringApplication.class, args);
+	}
+
+}
