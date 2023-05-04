da4210945f5ba02fc8e73ba7a854fba728b09002
Alberto Felipe <felipe0032sena@gmail.com>
2023-05-04 12:11:18 -0300

    Exception handling - delete

--- src/main/java/com/projectalberto/projectapispring/services/UserService.java
+++ src/main/java/com/projectalberto/projectapispring/services/UserService.java
@@ -2,8 +2,11 @@ package com.projectalberto.projectapispring.services;
 
 import com.projectalberto.projectapispring.entities.User;
 import com.projectalberto.projectapispring.repositories.UserRepository;
+import com.projectalberto.projectapispring.services.exceptions.DatabaseException;
 import com.projectalberto.projectapispring.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
+        try {
         repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
