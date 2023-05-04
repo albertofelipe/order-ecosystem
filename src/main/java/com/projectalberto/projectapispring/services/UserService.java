431734acc15f57edfb40b21385816c9513153224
Alberto Felipe <felipe0032sena@gmail.com>
2023-05-04 11:51:10 -0300

    Exception handling - findById

--- src/main/java/com/projectalberto/projectapispring/services/UserService.java
+++ src/main/java/com/projectalberto/projectapispring/services/UserService.java
@@ -2,6 +2,7 @@ package com.projectalberto.projectapispring.services;
 
 import com.projectalberto.projectapispring.entities.User;
 import com.projectalberto.projectapispring.repositories.UserRepository;
+import com.projectalberto.projectapispring.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
---
+++ src/main/java/com/projectalberto/projectapispring/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.projectalberto.projectapispring.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(Object id){
+        super("Resource not found. Id " + id);
+    }
+}
